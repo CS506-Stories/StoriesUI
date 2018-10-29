f4ee35e0774cde3f8e97f6a612a5bc3e110f8620
Sean Rice <seanrice2015@gmail.com>
2018-10-29 18:17:04 -0500

    Moved styles to routerStyles.js. Should eventually make this a navigation component, then create a master router

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Scene, Actions } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import routerStyles from './routerStyles';
 import Splash from '../modules/splash/Splash';
 import Profile from '../modules/profile/Profile';
 
@@ -13,8 +14,8 @@ const MainRouter = () => (
         key="splash"
         component={Splash}
         title="Splash"
-        renderRightButton={<Icon name="user-circle" size={30} style={{ paddingRight: 16 }} onPress={() => Actions.profile()} />}
-        renderLeftButton={<Icon name="camera" size={30} style={{ paddingLeft: 16 }} />}
+        renderRightButton={<Icon name="user-circle" size={30} style={routerStyles.rightButtonStyle} onPress={() => Actions.profile()} />}
+        renderLeftButton={<Icon name="camera" size={30} style={routerStyles.leftButtonStyle} />}
         initial
       />
 
