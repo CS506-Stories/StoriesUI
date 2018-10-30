a67ae702b3e965748b4484006ab12d66541df375
Sean Rice <seanrice2015@gmail.com>
2018-10-29 22:17:57 -0500

    Added settings cog to profile and new Settings view. This is just the layout and nothing is solidified or functional

---
+++ src/modules/settings/components/settingEntry/settingEntry.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Text, View, Switch,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const SettingEntry = (props) => (
+  <View style={styles.container}>
+    {
+      // TODO : replace fontsize with something scalable.
+    }
+    <Text style={{ fontSize:16 }} key={props.index}>{props.item}</Text>
+    <Switch />
+  </View>
+);
+
+SettingEntry.propTypes = {
+  index: PropTypes.number.isRequired,
+  item: PropTypes.string.isRequired,
+};
+
+export default SettingEntry;
---
+++ src/modules/settings/components/settingEntry/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 16,
+    height: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'powderblue',
+  },
+});
+
+export default styles;
