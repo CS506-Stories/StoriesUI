a7886939042bc60551e9b23a8f6b4fc8515068ab
heyyanting <yanting.liang96@gmail.com>
2018-12-02 16:05:04 -0600

    api

--- src/modules/mainfeed/components/like/Like.js
+++ src/modules/mainfeed/components/like/Like.js
@@ -1,36 +1,38 @@
 import * as React from 'react';
 import { Animated, View, Button } from 'react-native';
 // import Odometer from 'react-odometer';
-import { firestore } from '../../../../config/firebase';
+import { PropTypes } from 'prop-types';
+import { getReactionRate } from './api';
+import { auth, firestore } from '../../../../config/firebase';
 
 class Like extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        likes: [String],
-        reactionRate: number,
-        animation: Animated.Value
-      }
-    }
+    this.state = {
+      likes: [String],
+      reactionRate: 0,
+      animation: Animated.Value
+    };
+  }
   // counter: Odometer;
 
   componentWillMount() {
-		this.setState({ likes: this.props.likes, reactionRate: this.props.reactionRate });
-	}
+    this.setState({ likes: this.props.likes, reactionRate: this.props.reactionRate });
+  }
+
   // TODO : import user ID 
   toggle() {
+    const uid = auth.currentUser;
     const idx = this.state.likes.indexOf(uid);
     if (idx === -1) {
-
-      let likeArr = this.state.likes;
+      const likeArr = this.props.likes;
       likeArr.push(uid);
       this.setState({ likes: likeArr });
 
       // this.counter.increment;
       // Probably better to do this calculation somewhere else, like api.js
-      this.setState({ 
-        reactionRate: (firestore.Timestamp().toMillis() - timestamp)/this.state.likes.length
-      });
+      const newReactionRate = getReactionRate(likeArr.length, this.props.timestamp);
+      this.setState({ reactionRate: newReactionRate });
       const animation = new Animated.Value(0);
       this.setState({ animation });
       Animated.timing(
@@ -42,12 +44,14 @@ class Like extends React.Component {
         }
       ).start();
     } else {
-      likes.splice(uid, 1);
-      this.setState({ likes });
+      const likeArr = this.props.likes.splice(uid, 1);
+      this.setState({ likes: likeArr });
       // this.counter.decrement();
     }
     // TODO : move this into api.js
-    const dataRef = firestore.collection("posts").doc(post);
+
+    const dataRef = firestore.Firestore.collection("posts").doc(post);
+
     firestore.runTransaction(async transaction => {
       const dataDoc = await transaction.get(dataRef);
       const { likes } = dataDoc.data();
@@ -74,9 +78,8 @@ class Like extends React.Component {
 }
 
 Like.propTypes = {
-  likes: PropTypes.array.isRequired,
+  likes: PropTypes.instanceOf(Array).isRequired,
   post: PropTypes.string.isRequired,
-  reactionRate: PropTypes.number.isRequired,
   timestamp: PropTypes.number.isRequired,
 };
 
--- src/modules/mainfeed/components/like/api.js
+++ src/modules/mainfeed/components/like/api.js
@@ -1,1 +1,5 @@
+import { firestore } from '../../../../config/firebase';
 
+export function getReactionRate(number, timestamp) {
+  return (firestore.Timestamp().toMillis() - timestamp) / number;
+}
