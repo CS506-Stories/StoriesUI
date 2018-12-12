3e719a1af77ca8457e0fba97070b2806e682360e
heyyanting <yanting.liang96@gmail.com>
2018-12-12 10:13:51 -0600

    upload base64

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -100,7 +100,7 @@ export class Login extends Component {
               }
             <Form>
               <Item floatingLabel>
-                <Label style={styles.light}>Handle</Label>
+                <Label style={styles.light}>Email</Label>
                 <Input
                   returnKeyType="next"
                   keyboardAppearance="light"
